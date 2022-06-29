7c7534268342f54c283529f8bc38d4491d145015
purdyjen <purdyjen@me.com>
2022-06-29 14:01:45 -0400

    Added comments throughout code

--- script.js
+++ script.js
@@ -10,11 +10,6 @@ const symbolsEl = document.getElementById("symbols");
 const omitOptionEl = document.getElementById("omitOption");
 const omitSymbolsEl = document.getElementById("omitSymbols");
 
-// Character Strings
-const upperString = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-const lowerString = `abcdefghijklmnopqrstuvwxyz`;
-const numberString = `1234567890`;
-
 // If option to include symbols is checked, display option to specify which symbols to omit
 symbolsEl.addEventListener("click", function handleClick() {
   if (symbolsEl.checked) {
@@ -25,32 +20,38 @@ symbolsEl.addEventListener("click", function handleClick() {
 });
 
 const filterSymbols = () => {
+  // Grab value from Omit Symbols text field
   const omitSymbols = omitSymbolsEl.value;
-
+  // Set the starting value for symbolString
   let symbolString = `~\`!@#$%^&*()_-+={[}]|:;"'<,>.?/`;
+
   // Split symbolString into array symbolArr
   const symbolArr = symbolString.split("");
-  // If omitSymbols.length > 1, split string into array omitArr
+  // Split omitSymbols string into array omitArr
   const omitArr = omitSymbols.split("");
-  if (omitArr.length > 1) {
-    // search symbolArr for omitArr[i]
-    // if found, splice symbolArr at k
 
+  // If the length of omitArr is greater than 1 (i.e., not an empty array), run this code
+  if (omitArr.length > 1) {
+    // compare omitArr[i] with symbolArr[k]
+    // Because this is filtering out unwanted symbols, it is necessary for symbolArr to be the inner loop as its value will be changed every time a match is found
     for (let i = 0; i < omitArr.length; i++) {
       for (let k = 0; k < symbolArr.length; k++) {
+        // if match, splice symbolArr at k
         if (omitArr[i] === symbolArr[k]) {
           symbolArr.splice(k, 1);
         }
       }
     }
-
+    // After filter is complete, convert symbolArr back into a string
     symbolString = symbolArr.join("");
     return symbolString;
   } else {
+    // If the length of omitArr is not greater than 1 (i.e., an empty array), then no filter is necessary
     return symbolString;
   }
 };
 
+// Function to generate a random number between two numbers with min being inclusive and max being exclusive
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -58,38 +59,60 @@ const getRandomInt = (min, max) => {
 };
 
 const buildArray = () => {
+  // Grab the checked values for customization options
   const hasUpper = uppercaseEl.checked;
   const hasLower = lowercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
-  const symString = filterSymbols();
 
-  let pwStr = "";
+  // Uppercase, Lowercase, and Number Strings
+  const upperString = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+  const lowerString = `abcdefghijklmnopqrstuvwxyz`;
+  const numberString = `1234567890`;
 
+  // Build string of possible characters to use in generating the password
+  // Initially, set to an empty string
+  let charStr = "";
+
+  // If hasUpper is true, then add upperString to charStr
   if (hasUpper) {
-    pwStr += upperString;
+    charStr += upperString;
   }
 
+  // If hasLower is true, then add lowerString to charStr
   if (hasLower) {
-    pwStr += lowerString;
+    charStr += lowerString;
   }
 
+  // If hasNumber is true, then add numberString to charStr
   if (hasNumber) {
-    pwStr += numberString;
+    charStr += numberString;
   }
 
+  // If hasSymbol is true, then build symbolString and add to charStr
   if (hasSymbol) {
-    pwStr += symString;
+    // Build Symbol String
+    const symString = filterSymbols();
+    charStr += symString;
   }
 
-  const pwArr = pwStr.split("");
-  return pwArr;
+  // Convert charStr to array
+  const charArr = charStr.split("");
+  // Return final character array that will be used by the generatePassword function
+  return charArr;
 };
 
 const generatePassword = () => {
+  // Grab the desired length of password (either user-specified or the default value of 20)
   const length = lengthEl.value;
+  // Create variable to hold the generated password
   let generatedPassword = "";
+  
+  // Calls the buildArray function 
   const arr = buildArray();
+
+  // A for loop that will run the number of length times
+    // Each iteration will call the function to generate a random number between 0 and the length of the final/customized character array
   for (let i = 0; i <= length - 1; i++) {
     let index = getRandomInt(0, arr.length);
     let newChar = arr[index];
@@ -98,17 +121,18 @@ const generatePassword = () => {
   return (newPw.innerHTML = generatedPassword);
 };
 
+// Calls generatePassword() on click
 genBtn.addEventListener("click", (e) => {
   generatePassword();
 });
 
+// Copies the password to clipboard
 function copyToClipboard() {
   /* Get the text field */
   var copyText = newPw.innerHTML;
 
   /* Copy the text inside the text field */
   navigator.clipboard.writeText(copyText);
-
 }
 
 copyPw.addEventListener("click", (e) => {
