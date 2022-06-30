fc0c17d459cf4210aa3a0fb7172af21cee4757b4
purdyjen <purdyjen@me.com>
2022-06-30 14:17:28 -0400

    Added option to specify which symbols can be used

--- script.js
+++ script.js
@@ -7,14 +7,37 @@ const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
 const numbersEl = document.getElementById("numbers");
 const symbolsEl = document.getElementById("symbols");
+const symbolRadiosEl = document.getElementById("symbolRadios");
 const omitOptionEl = document.getElementById("omitOption");
 const omitSymbolsEl = document.getElementById("omitSymbols");
-
+const includeOptionEl = document.getElementById("includeOption");
+const includeSymbolsEl = document.getElementById("includeSymbols");
+const incSymb = document.getElementById("incSymb");
+const omitSymb = document.getElementById("omitSymb");
 // If option to include symbols is checked, display option to specify which symbols to omit
 symbolsEl.addEventListener("click", function handleClick() {
   if (symbolsEl.checked) {
-    omitOptionEl.style.display = "block";
+    symbolRadiosEl.style.display = "block";
   } else {
+    symbolRadiosEl.style.display = "none";
+    includeOptionEl.style.display = "none";
+    omitOptionEl.style.display = "none";
+    incSymb.checked = false;
+    omitSymb.checked = false;
+    omitSymbolsEl.value = '';
+    includeSymbolsEl.value = '';
+  }
+});
+
+symbolRadiosEl.addEventListener("click", function handleClick() {
+  if (incSymb.checked) {
+    includeOptionEl.style.display = "block";
+    omitOptionEl.style.display = "none";
+  } else if (omitSymb.checked) {
+    omitOptionEl.style.display = "block";
+    includeOptionEl.style.display = "none";
+  } else {
+    includeOptionEl.style.display = "none";
     omitOptionEl.style.display = "none";
   }
 });
@@ -22,6 +45,7 @@ symbolsEl.addEventListener("click", function handleClick() {
 const filterSymbols = () => {
   // Grab value from Omit Symbols text field
   const omitSymbols = omitSymbolsEl.value;
+  const includeSymbols = includeSymbolsEl.value;
   // Set the starting value for symbolString
   let symbolString = `~\`!@#$%^&*()_-+={[}]|:;"'<,>.?/`;
 
@@ -44,6 +68,9 @@ const filterSymbols = () => {
     }
     // After filter is complete, convert symbolArr back into a string
     symbolString = symbolArr.join("");
+    return symbolString;
+  } else if (includeSymbols.length > 0) {
+    symbolString = includeSymbols;
     return symbolString;
   } else {
     // If the length of omitArr is not greater than 1 (i.e., an empty array), then no filter is necessary
@@ -107,12 +134,12 @@ const generatePassword = () => {
   const length = lengthEl.value;
   // Create variable to hold the generated password
   let generatedPassword = "";
-  
-  // Calls the buildArray function 
+
+  // Calls the buildArray function
   const arr = buildArray();
 
   // A for loop that will run the number of length times
-    // Each iteration will call the function to generate a random number between 0 and the length of the final/customized character array
+  // Each iteration will call the function to generate a random number between 0 and the length of the final/customized character array
   for (let i = 0; i <= length - 1; i++) {
     let index = getRandomInt(0, arr.length);
     let newChar = arr[index];
